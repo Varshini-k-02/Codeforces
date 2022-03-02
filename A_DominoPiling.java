38d163ba761daf1e7bad35fe5866b6dd0f1083c6
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-03-02 17:43:13 +0530

    Day 2

---
+++ A_DominoPiling.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class A_DominoPiling{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        int m=scan.nextInt();
+        int x=(n*m)/2;
+        System.out.println(x);
+    }
+}
